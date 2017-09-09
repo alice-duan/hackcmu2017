f727f680b4592591e16618978a3a46abf44b931d
Jared Moore <jmoore1@andrew.cmu.edu>
2017-09-08 23:53:47 -0400

    Added connect back on initial connection

--- public/p2p.js
+++ public/p2p.js
@@ -4,56 +4,80 @@ var peer = new Peer({ key: "2nnnwv66dinhr529" });
 
 console.log("peer data", peer);
 
-peer.on("open", id => {
-  console.log("My peer ID is: " + id);
-  document.getElementById("id-here").innerHTML = id;
+let id;
+peer.on("open", peerId => {
+  console.log("My peer ID is: " + peerId);
+  document.getElementById("id-here").innerHTML = peerId;
+
+  id = peerId;
 });
+
+const connections = [];
+
+// TODO: disconnect!?
 
 peer.on("connection", conn => {
   console.log("got connection: from " + conn.peer, conn);
 
-  const newP = document.createElement("p");
-  newP.textContent = conn.peer;
-  document.getElementById("connections-here").appendChild(newP);
+  // connect back if not already connected
+  if (!connections.find(c => c.peer === conn.peer)) {
+    connections.push(peer.connect(conn.peer));
+    addChatMessage(conn.peer + " connected!");
+    console.log("Connected back to " + conn.peer);
+  }
 
   // Receive messages
   conn.on("data", data => {
     console.log("Received", data);
 
-    const newP = document.createElement("p");
-    newP.textContent = data;
-    document.getElementById("chat").appendChild(newP);
+    switch (data.type) {
+      default:
+        addChatMessage(conn.peer + ": " + data.content);
+        break;
+    }
   });
 
   // Send messages
-  conn.send("Hello!");
+  //conn.send("Hello!");
 });
 
-const connections = [];
 //connects to peer, updates connections displayed, and
 //connects to all of the peer's connections
 function connect() {
-  const peerId = document.getElementById("peerId").value
+  const peerId = document.getElementById("peerId").value;
 
   console.log("trying to connect to", peerId);
   const conn = peer.connect(peerId);
   console.log("connection:", conn);
 
-  const newP = document.createElement("p");
-  newP.textContent = peerId;
-  document.getElementById("connections-here").appendChild(newP);
+  addChatMessage("Connected to " + conn.peer + "!"); // TODO: server messages should be different
+  addConnection(conn.peer);
 
   connections.push(conn);
 }
 
 function send() {
   connections.forEach(conn => {
-    const data = document.getElementById("message").value
+    const { value } = document.getElementById("message");
 
-    conn.send(data);
+    conn.send({ type: "message", content: value });
 
-    const newP = document.createElement("p");
-    newP.textContent = data;
-    document.getElementById("chat").appendChild(newP);
+    addChatMessage("you: " + value);
   });
 }
+
+// add msg to chat box
+const addChatMessage = msg => append("chat", msg);
+
+// add peer name to connections list
+const addConnection = name => append("connections-here", name);
+
+const append = (id, msg, elem = "p") => {
+  const element = document.createElement(elem);
+  element.textContent = msg;
+  document.getElementById(id).appendChild(element);
+};
+
+setInterval(() => {
+  console.log("Current connections:", connections.slice(0));
+}, 5000);
