ef5a121e386d03a08dbd6f37453eab332b73def8
Alice Duan <aduan1@andrew.cmu.edu>
2017-09-08 23:16:27 -0400

    made connections mutual

--- public/p2p.js
+++ public/p2p.js
@@ -6,10 +6,15 @@ console.log("peer data", peer);
 
 peer.on("open", id => {
   console.log("My peer ID is: " + id);
+  document.getElementById("id-here").innerHTML = id;
 });
 
 peer.on("connection", conn => {
   console.log("got connection: from " + conn.peer, conn);
+
+  const newP = document.createElement("p");
+  newP.textContent = conn.peer;
+  document.getElementById("connections-here").appendChild(newP);
 
   // Receive messages
   conn.on("data", data => {
@@ -25,16 +30,30 @@ peer.on("connection", conn => {
 });
 
 const connections = [];
+//connects to peer, updates connections displayed, and
+//connects to all of the peer's connections
 function connect() {
-  console.log("trying to connect to", document.getElementById("peerId").value);
-  const conn = peer.connect(document.getElementById("peerId").value);
+  const peerId = document.getElementById("peerId").value
+
+  console.log("trying to connect to", peerId);
+  const conn = peer.connect(peerId);
   console.log("connection:", conn);
+
+  const newP = document.createElement("p");
+  newP.textContent = peerId;
+  document.getElementById("connections-here").appendChild(newP);
 
   connections.push(conn);
 }
 
 function send() {
   connections.forEach(conn => {
-    conn.send(document.getElementById("message").value);
+    const data = document.getElementById("message").value
+
+    conn.send(data);
+
+    const newP = document.createElement("p");
+    newP.textContent = data;
+    document.getElementById("chat").appendChild(newP);
   });
 }
